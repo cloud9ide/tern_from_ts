6a0107e602b0d044fe1753533cf31f52cf5fb95a
Lennart Kats <lclkats@gmail.com>
2015-02-10 11:39:57 +0100

    Remove failed sigs that had zero exit code

--- from_ts.sh
+++ from_ts.sh
@@ -9,11 +9,21 @@ readonly TARGET=$2
 
 echo -n $TARGET
 
+reportError() {
+    echo " - $(cat | grep Error:)"
+}
+
 if ! ERROR=$($MY_DIR/node_modules/tern/bin/from_ts "$SOURCE" 2>&1 | sed 's/\[object Object\]/?/g' >$TARGET.tmp); then
     rm $TARGET.tmp
-    DETAILS=$(echo "$ERROR" | grep Error:)
-    echo " - $DETAILS"
+    echo "$ERROR" | reportError
 else
+    if ! node -e 'JSON.parse(require("fs").readFileSync("'$TARGET.tmp'").toString())' 2>/dev/null; then
+        # Error reported with zero exit code
+        cat $TARGET.tmp | reportError
+        rm $TARGET.tmp
+        exit
+    fi
+
     mv $TARGET.tmp $TARGET
     echo
 fi
