063afc5a645ff0f0c57c40eb3866e444f8363ee5
Lennart Kats <lclkats@gmail.com>
2015-12-23 16:11:08 +0000

    Rename sigs to defs

--- from_ts_all.sh
+++ from_ts_all.sh
@@ -5,25 +5,25 @@ npm install
 MY_DIR=$(cd $(dirname $BASH_SOURCE); pwd)
 
 cd $MY_DIR
-[ -e sigs_ts ] || git clone https://github.com/borisyankov/DefinitelyTyped.git sigs_ts
-cd sigs_ts
+[ -e defs_ts ] || git clone https://github.com/borisyankov/DefinitelyTyped.git defs_ts
+cd defs_ts
 git pull
-[ -e sigs/revision.txt ] && git checkout $(cat sigs/revision.txt)
+[ -e defs/revision.txt ] && git checkout $(cat defs/revision.txt)
 REVISION=$(git rev-parse HEAD)
 
 cd $MY_DIR
-mkdir -p sigs
-cd sigs
+mkdir -p defs
+cd defs
 echo $REVISION > revision.txt
-cp ../sigs_ts/LICENSE .
+cp ../defs_ts/LICENSE .
 
 if ! which parallel &> /dev/null; then
     echo "Please install GNU parallel" >&2
     return 1
 fi
 
-ls ../sigs_ts | parallel '
-    DIR=../sigs_ts/{1}
+ls ../defs_ts | parallel '
+    DIR=../defs_ts/{1}
     find_main_file() {
         local DIR=$1
         local NAME=$2
@@ -57,27 +57,27 @@ ls ../sigs_ts | parallel '
     done
 '
 
-rm -f sigs/yosay.json
+rm -f defs/yosay.json
 
 {
-    echo '{"sigs":{'
-    for D in $(cd $MY_DIR/sigs; ls); do
+    echo '{"defs":{'
+    for D in $(cd $MY_DIR/defs; ls); do
         if [[ "$D" =~ ^_ ]]; then
             continue
         fi
         echo -n '"'${D%.json}'": {'
         echo -n '"main": "'$D'",'
-        PROJECT=$(grep -s 'Project: http' $MY_DIR/sigs_ts/${D%.json}/${D%.json}.d.ts | head -1 | grep -o 'http:.*' | tr -d '\r' || :)
+        PROJECT=$(grep -s 'Project: http' $MY_DIR/defs_ts/${D%.json}/${D%.json}.d.ts | head -1 | grep -o 'http:.*' | tr -d '\r' || :)
         if [ "$PROJECT" ]; then
             echo "PROJECT: $PROJECT" >&2
             echo -n '"url": "'$PROJECT'",'
         fi
         echo -n '"extra": {'
-        for E in $(cd $MY_DIR/sigs; ls _${D%.json}* 2>/dev/null); do
+        for E in $(cd $MY_DIR/defs; ls _${D%.json}* 2>/dev/null); do
             echo -n '"'${E%.json}'": "'$E'",'
         done
         echo -n '}},'
     done
     echo '}}'
-} | sed 's/"extra": {}//g; s/, *}/}/g' > $MY_DIR/sigs/__list.tmp
-mv $MY_DIR/sigs/__list.tmp $MY_DIR/sigs/__list.json
+} | sed 's/"extra": {}//g; s/, *}/}/g' > $MY_DIR/defs/__list.tmp
+mv $MY_DIR/defs/__list.tmp $MY_DIR/defs/__list.json
